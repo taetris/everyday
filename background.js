34ae5531ef0a7678ec8235abaac6ac6b9ae4fdee
taetris <triptis1235@gmail.com>
2024-06-10 13:41:52 +0545

    Timer made into class and added

--- background.js
+++ background.js
@@ -1,59 +1,97 @@
-let timerState = {
-    running: false,
-    minutes: 0,
-    seconds: 5,
-    isBreak: false,
+class Timer {
+    constructor(minutes = 25, breakMinutes = 1) {
+        this.state = {
+            running: false,
+            minutes: minutes,
+            seconds: 4,
+            isBreak: false,
+            breakMinutes: breakMinutes,
+            workMinutes: minutes,
+            isTimeUp: false,
+            message: "Start a 25min session",
+        };
+        this.timerInterval = null;
+    }
+
+    startTimer() {
+        console.log(this.state);
+        if(this.state.running) return;
+        this.state.running = true;
+        if (this.state.isBreak) {
+            this.state.minutes = this.state.breakMinutes;
+            this.state.message = "5min break";
+        }else{
+            this.state.minutes = this.state.workMinutes;
+            this.state.message = "25min session";
+        }
+        this.timerInterval = setInterval(() => {
+            const countdown = () => {
+                
+                if (this.state.seconds === 0) {
+                    if (this.state.minutes === 0) {
+                        return true;
+                        
+                    } else {
+                        this.state.seconds = 59;
+                        this.state.minutes--;
+                    }
+                } else {
+                    this.state.seconds--;
+                }
+
+                if(countdown){
+                    this.state.isTimeUp = !this.state.isTimeUp;
+                    this.state.running = false;
+                    this.state.isBreak = !this.state.isBreak;
+
+                    clearInterval(this.timerInterval);
+                    console.log("exit", this.state);
+                    if(this.state.isTimeUp){
+                        this.state.message = "Times up!";
+                        this.state.isTimeUp = !this.state.isTimeUp
+                        chrome.runtime.sendMessage({ action: "timeUp", timerState: this.state });
+                    }
+                }
+            }
+
+            chrome.runtime.sendMessage({ action: "update", timerState: this.state });
+        }, 1000);
+    }
+
+    pauseTimer() {
+        if (!this.state.running) return;
+        this.state.running = false;
+        
+        this.state.message = "Paused";
+        chrome.runtime.sendMessage({ action: "timePaused", timerState: this.state });
+    }
+
+    resetTimer() {
+        if (!this.state.running) return;
+        this.state.running = false;
+        clearInterval(this.timerInterval);
+        this.state.seconds = 0;
+        this.state.minutes = this.state.workMinutes;
+        this.state.isBreak = false;
+        this.state.message = "Start a 25min session, reset";
+        chrome.runtime.sendMessage({ action: "reset", timerState: this.state });
+    }
 }
-let timerInterval;
 
-
-console.log(timerState);
+const obj = new Timer(1, 1);
+console.log("obj")
 chrome.runtime.onMessage.addListener((message, sender, sendResponse)=>{
     if(message.action == "start"){
-        startTimer();
-
-    }else if(message.action === "pause"){
-        pauseTimer();
         
-    }else if(message.action === "reset"){
-        resetTimer();
+        if(obj.state.running === false){
+            obj.startTimer();
+        }else{
+            obj.pauseTimer();
+        } 
+    }   
+        
+    else if(message.action === "reset"){
+        obj.resetTimer();
 
-    }else if(message.action === "getTimerState"){
-        sendResponse(timerState);
     }
 });
-
-
-const startTimer = () => {   
-    console.log(timerState);
-    if (timerState.running) return;
-    timerState.running = true;
-
-    timerInterval = setInterval(() => {
-        if(timerState.seconds === 0){
-            if(timerState.minutes === 0){
-                timerState.running = false;
-                timerState.isBreak = !timerState.isBreak;
-                timerState.minutes = timerState.isBreak ? 1 : 25;
-
-                clearInterval(timerInterval);
-                console.log("exit",timerState);
-                chrome.runtime.sendMessage({action:"timeUp", isBreak: timerState.isBreak});
-            }else{
-                timerState.seconds = 59;
-                timerState.minutes--;
-            }   
-        }else{
-            timerState.seconds--;
-        }
-        chrome.runtime.sendMessage({ action: "update", timerState });
-  }, 1000);
-        
-    }
-
-    const pauseTimer = () => {
-        timerState.running = false;
-        clearInterval(timerInterval);
-
-        chrome.runtime.sendMessage({action:"timePaused", isBreak: timerState.isBreak});
-    }
--- popup.js
+++ popup.js
@@ -1,41 +1,52 @@
 const timer = document.getElementById('timer');
 const start = document.getElementById('start');
-const pause = document.getElementById('pause');
-const reset = document.getElementById('reset');``
+
+const reset = document.getElementById('reset');
 const banner = document.getElementById('banner');
 
+
+
 start.addEventListener('click', () => {
-    
-    if(timerState.isBreak === true){
-        banner.innerHTML = "Break time";
-        timer.innerHTML = "05:00";
-    }
-    else{
-        banner.innerHTML = "Work time";
-        timer.innerHTML = "25:00";
-    } 
     chrome.runtime.sendMessage({ action: "start" });
+    console.log("mes sent")
   });
+
+ 
+reset.addEventListener('click', () => {
+    
+    chrome.runtime.sendMessage({ action: "reset" });
+});
+
 
 
 
 chrome.runtime.onMessage.addListener((message) => {
+    banner.innerHTML = message.timerState.message; 
+
 if (message.action === "update") {
-    console.log("updating...")
-    
-    timer.innerHTML = message.timerState.minutes.toLocaleString(undefined,{minimumIntegerDigits: 2}) + ":" + message.timerState.seconds.toLocaleString(undefined,{minimumIntegerDigits: 2});
+    const { minutes, seconds } = message.timerState;
+    timer.innerHTML = minutes.toLocaleString(undefined, { minimumIntegerDigits: 2 }) + ":" + seconds.toLocaleString(undefined, { minimumIntegerDigits: 2 });
+   
 } else if (message.action === "timeUp") {
     
-    banner.innerHTML = "Times up!";
+    
+    let mySound = new Audio('alarm.wav');
+    mySound.play();
+    // playAlarm();
 }
 
 else if (message.action === "timePaused") {
-    banner.innerHTML = "Paused";
+    
     chrome.runtime.sendMessage({ action: "pause" });
 }
 else if(message.action === "reset"){
-    banner.innerHTML = "Start a 25min session";
+    
+    timer.innerHTML = message.timerState.minutes.toLocaleString(undefined,{minimumIntegerDigits: 2}) + ":" + message.timerState.seconds.toLocaleString(undefined,{minimumIntegerDigits: 2});
 }
+
+    
+banner.innerHTML = message.timerState.message; 
+
 });
 
 
